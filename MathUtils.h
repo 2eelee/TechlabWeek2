c5fce2f32c2fc137f40f830955406e59cbbd0ca9
Sangyun Jeong <jsangyun1039@gmail.com>
2026-09-07 20:14:11 +0900

    MathUtils로 파일명 수정 및 Determinant 계산 함수추가

--- MathUtils.h
+++ MathUtils.h
@@ -5,7 +5,7 @@
 
 namespace MathUtils 
 {
-	FVector2 ScreenToNDC(
+	inline FVector2 ScreenToNDC(
 		const FIntPoint& screenPosition, 
 		float viewportWidth, 
 		float viewportHeight
@@ -15,5 +15,14 @@ namespace MathUtils
 			(2.0f * screenPosition.X) / viewportWidth - 1.0f, 
 			1.0f - (2.0f * screenPosition.Y) / viewportHeight 
 		};
+	}
+
+	inline float Det3x3(
+		float a, float b, float c,
+		float d, float e, float f,
+		float g, float h, float i
+	)
+	{
+		return a * (e * i - f * h) - b * (d * i - f * g) + c * (d * h - e * g);
 	}
 }
