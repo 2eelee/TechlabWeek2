6e08bcf123e9418b9e6108b189cbd2f1a89d6616
2eelee <yoo.hyuseo.dev@gmail.com>
2026-09-08 21:20:54 +0900

    FVector2 연산 추가

--- FVector2.h
+++ FVector2.h
@@ -4,4 +4,19 @@ struct FVector2
 {
 	float X;
 	float Y;
+
+	float Dot(const FVector2& other) const { return X * other.X + Y * other.Y; }
+
+	float Length() const { return sqrtf(X * X + Y * Y); }
+
+	FVector2 operator-(const FVector2& other) const {
+		return FVector2(X - other.X, Y - other.Y);
+	}
+
+	FVector2 Normalize() const
+	{
+		float length = Length();
+		return length > 0.000001f ? FVector2{ X / length, Y / length } : FVector2{ 0.0f, 0.0f };
+	}
+
 };
